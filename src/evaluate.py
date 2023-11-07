7c159b8743ba8dd92a62d061e1e58b76c01f6c93
Electro98 <geny_turov@mail.ru>
2023-11-07 21:02:38 +0700

    Add loss evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,21 +1,55 @@
-from joblib import load
 import json
 from pathlib import Path
 
-from sklearn.metrics import accuracy_score
+import numpy as np
 
-from train import load_data
+import tensorflow as tf
+
+from tqdm import tqdm
+
+from train import create_losses, IMAGE_SIZE, BATCH_SIZE, LATENT_SIZE
 
 
-def main(repo_path):
-    test_csv_path = repo_path / "data/prepared/test.csv"
-    test_data, labels = load_data(test_csv_path)
-    model = load(repo_path / "model/model.joblib")
-    predictions = model.predict(test_data)
-    accuracy = accuracy_score(labels, predictions)
-    metrics = {"accuracy": accuracy}
-    accuracy_path = repo_path / "metrics/accuracy.json"
-    accuracy_path.write_text(json.dumps(metrics))
+def main(repo_path: Path):
+    generator = tf.keras.models.load_model(
+        repo_path / "model/generator.h5", compile=False)
+    discriminator = tf.keras.models.load_model(
+        repo_path / "model/discriminator.h5", compile=False)
+    discriminator_loss, generator_loss = create_losses()
+
+    # loading data
+    data_dir = repo_path / "data/raw/test"
+
+    test_dataset = tf.keras.utils.image_dataset_from_directory(
+        data_dir, image_size=(IMAGE_SIZE, IMAGE_SIZE), 
+        batch_size=BATCH_SIZE, label_mode=None)
+    test_dataset = test_dataset.map(lambda x: (x - 127.5) / 127.5)
+    print("Data loaded!")
+    
+    gen_losses, desc_losses = [], []
+    for image_batch in tqdm(test_dataset, desc="Evaluation"):
+        noise = tf.random.normal([BATCH_SIZE, LATENT_SIZE])
+        generated_images = generator(noise, training=False)
+        real_output = discriminator(image_batch, training=False)
+        fake_output = discriminator(generated_images, training=False)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+        gen_losses.append(gen_loss.numpy())
+        desc_losses.append(disc_loss.numpy())
+
+    # Saving results
+    accuracy_path = repo_path / "metrics/metrics.json"
+    accuracy_path.write_text(json.dumps({
+        "generator": {
+            "loss_mean": float(np.mean(gen_losses)),
+            "loss_std": float(np.std(gen_losses)),
+        },
+        "discriminator": {
+            "loss_mean": float(np.mean(desc_losses)),
+            "loss_std": float(np.std(desc_losses)),
+        },
+    }, indent=4))
 
 
 if __name__ == "__main__":
